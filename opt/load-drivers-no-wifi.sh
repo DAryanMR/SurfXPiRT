24693780a57141d5d21fffb2d83e0f0b648441aa
DAryanMR <surfxpirt@gmail.com>
2023-05-05 17:58:22 +0600

    updated load-drivers-no-wifi.sh

--- opt/load-drivers-no-wifi.sh
+++ opt/load-drivers-no-wifi.sh
@@ -7,7 +7,6 @@ for module in \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/gpu/drm/i2c/*.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/gpu/drm/ttm/ttm.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/i2c/busses/i2c-cros-ec-tunnel.ko \
-    /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/input/mouse/elan_i2c.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/media/dvb-frontends/*.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/media/dvb-frontends/cxd2880/cxd2880.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/media/dvb-frontends/drx39xyj/drx39xyj.ko \
