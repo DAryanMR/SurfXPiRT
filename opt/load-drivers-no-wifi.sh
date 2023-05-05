6c24d918de7a490e5d4ea682310c439851c304d1
DAryanMR <surfxpirt@gmail.com>
2023-05-05 18:18:49 +0600

    updated load-drivers-no-wifi.sh

--- opt/load-drivers-no-wifi.sh
+++ opt/load-drivers-no-wifi.sh
@@ -3,7 +3,6 @@
 echo "Loading modules..."
 for module in \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/bluetooth/*.ko \
-    /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/gpu/drm/*.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/gpu/drm/i2c/*.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/gpu/drm/ttm/ttm.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/i2c/busses/i2c-cros-ec-tunnel.ko \
@@ -12,7 +11,6 @@ for module in \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/media/dvb-frontends/drx39xyj/drx39xyj.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/media/spi/cxd2880-spi.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/media/tuners/*.ko \
-    /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/platform/chrome/*.ko \
     /lib/modules/5.17.0-rc3-next-20220207-g5bd2d473f01f/kernel/drivers/thermal/tegra/*.ko
