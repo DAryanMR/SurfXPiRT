82cad89bc571efd5fc1d8e56116cf39b255ba57f
DAryanMR <132191670+DAryanMR@users.noreply.github.com>
2023-05-10 20:08:07 +0600

    removed update-surfxpirt

--- opt/custom-scripts/install-packages.sh
+++ opt/custom-scripts/install-packages.sh
@@ -158,9 +158,10 @@ rm -rf /boot/config.txt
 cp /boot/firmware/opt/config.txt /boot/
 
 # Copy update-surfxpirt to /usr/local/bin
-echo "Copying surfxpirt-update-manager..."
-cp /boot/firmware/opt/update_manager/* /usr/local/bin
-echo "Now you can run 'sudo update-surfxpirt' (after cloning to eMMC) to get latest updates for your Surface RT"
+rm -rf /usr/local/bin/update-surfxpirt
+#echo "Copying surfxpirt-update-manager..."
+#cp /boot/firmware/opt/update_manager/* /usr/local/bin
+#echo "Now you can run 'sudo update-surfxpirt' (after cloning to eMMC) to get latest updates for your Surface RT"
 
 # Generating initrd.img
 echo "Generating 5.17-rc3 initrd.img"
