3aa9ec83e913b615fe8eefea33e7ae656300d49b
DAryanMR <132191670+DAryanMR@users.noreply.github.com>
2023-05-10 11:38:33 +0600

    enabled load-modules on-start

--- opt/opt-post-clone/bin/on-start.sh
+++ opt/opt-post-clone/bin/on-start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh -e
 
-# echo "CSS1: Loading Modules"
-# sh /etc/load-supp-drivers.sh
+echo "CSS1: Loading Modules"
+sh /etc/load-supp-drivers.sh
 # echo ""
 # echo "CSS2: Network Attempt"
 # sh /etc/rc-local.sh
