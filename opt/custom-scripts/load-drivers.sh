e8262e3821db075c55b6c08101e9e61e0f74152b
DAryanMR <132191670+DAryanMR@users.noreply.github.com>
2023-05-12 12:34:20 +0000

    added sleep,clear

--- opt/custom-scripts/load-drivers.sh
+++ opt/custom-scripts/load-drivers.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+echo "executing load-drivers.sh"
 # Run pre-driver script first
 echo "Running pre-driver script"
 sh /boot/firmware/opt/custom-scripts/pre-driver.sh
@@ -7,5 +8,8 @@ sh /boot/firmware/opt/custom-scripts/pre-driver.sh
 # Then run post-driver script
 echo "Running post-driver script"
 sh /boot/firmware/opt/custom-scripts/post-driver.sh
- 
+
+sleep 2
+clear
+
 exit 0
